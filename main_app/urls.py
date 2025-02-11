53b41907854c87b9fe162c923f95ff4a297f4e66
Alla-korn <163337287+Alla-korn@users.noreply.github.com>
2025-02-11 21:26:20 +0300

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,1 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from django.views.decorators.cache import cache_page
 
-from main_app.apps import MainAppConfig
-from main_app.views import get_main_page
-
-app_name = MainAppConfig.name
-urlpatterns = [
-                  path('', get_main_page, name='main_page'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
